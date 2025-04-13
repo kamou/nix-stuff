651db1bb3964ce9f136c69581e019433798226d5
Ayman Khamouma <noreply@ak42.io>
2025-04-13 17:03:11 +0200

    Update bootstrap for full system + home-manager installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,25 @@
 
 set -e
 
+if ! command -v git &>/dev/null; then
+    echo "git could not be found, run \`nix-shell -p git\` prior to running this script"
+    exit 1
+fi
+
+if [ ! -f /home/ak42/.config/sops/age/keys.txt ]; then
+    echo "No age keys found, please create ~/.config/sops/age/keys.txt"
+    exit 1
+fi
+
+if [ ! -d /etc/nixos/original/ ]; then
+    sudo mkdir -p /etc/nixos/original/
+    sudo mv /etc/nixos/configuration.nix /etc/nixos/hardware-configuration.nix /etc/nixos/original/
+fi
+
+sudo curl -L https://raw.githubusercontent.com/kamou/nix-stuff/main/configuration.nix -o /etc/nixos/configuration.nix
+sudo nixos-rebuild switch
+
 # enable home-manager and unstable nixpkgs channels
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz home-manager
 
